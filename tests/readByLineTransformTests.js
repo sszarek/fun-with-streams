97e40cdee752c39a2dff63448ff719c00a7157ae
Stefan Szarek <stefan.szarek@hotmail.com>
2016-04-30 20:56:29 +0200

    Lines are being push properly to the output

--- tests/readByLineTransformTests.js
+++ tests/readByLineTransformTests.js
@@ -8,38 +8,36 @@ const SMALL_CHUNKS = 5;
 
 describe('ReadByLineTransform tests', function () {
     it('should not push data when empty string passed', function (done) {
-        let pushedLines = [];
-        new MockReadableStream('', SMALL_CHUNKS, { encoding: 'utf8' })
-            .pipe(new ReadByLineTransform())
-            .on('data', data => pushedLines.push(data))
-            .on('end', () => {
-                assert.equal(pushedLines.length, 0);
-                done();
-            });
+        runReadByLineTransformTest('', SMALL_CHUNKS, pushedLines => {
+            assert.equal(pushedLines.length, 0);
+            done();
+        });
     });
 
     it('should push one line of data when string is not longer than chunk size and has no line breaks', function (done) {
-        let pushedLines = [];
-        new MockReadableStream('line', SMALL_CHUNKS, { encoding: 'utf8' })
-            .pipe(new ReadByLineTransform())
-            .on('data', data => {
-                pushedLines.push(data);
-            })
-            .on('end', () => {
-                assert.equal(pushedLines.length, 1);
-                assert.equal(pushedLines[0], 'line');
-                done();
-            });
+        runReadByLineTransformTest('line', SMALL_CHUNKS, pushedLines => {
+            assert.equal(pushedLines.length, 1);
+            assert.equal(pushedLines[0], 'line');
+            done();
+        });
     });
 
     it('should push two lines when string is not longer than chunk size and has line break', function (done) {
-        let pushedLines = [];
-        new MockReadableStream('li\nne', SMALL_CHUNKS, { encoding: 'utf8' })
-            .pipe(new ReadByLineTransform())
-            .on('data', data => pushedLines.push(data))
-            .on('end', (data) => {
-                assert.equal(pushedLines.length, 2);
-                done();
-            });
+        runReadByLineTransformTest('li\nne', SMALL_CHUNKS, pushedLines => {
+            assert.equal(pushedLines.length, 2);
+            assert.equal(pushedLines[0], 'li');
+            assert.equal(pushedLines[1], 'ne');
+            done();
+        });
     });
 });
+
+function runReadByLineTransformTest(input, chunkSize, callback) {
+    let pushedLines = [];
+    new MockReadableStream(input, chunkSize, { encoding: 'utf8' })
+        .pipe(new ReadByLineTransform())
+        .on('data', data => pushedLines.push(data))
+        .on('end', () => {
+            callback(pushedLines);
+        });
+}
